946da1fb67f4169d9868629b7f2d527761ddb71a
kiquetal <kiquetal@gmail.com>
2026-03-13 09:52:25 -0300

    preparing the change of the layout and css

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,18 @@ import react from '@astrojs/react';
 
 import node from '@astrojs/node';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   output: 'server',
+
   adapter: node({
     mode: 'standalone'
-  })
+  }),
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
